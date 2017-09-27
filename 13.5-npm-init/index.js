e23350bef4a3b069f4d55d99e25d704d41ec590a
Piotr Iwaniuk <piwaniuk@esri.pl>
2017-09-27 18:58:08 +0200

    emitter set

--- 13.5-npm-init/index.js
+++ 13.5-npm-init/index.js
@@ -1,21 +1,36 @@
+var EventEmitter = require("events").EventEmitter;
 var OSinfo = require('./modules/OSInfo');
 
-process.stdin.setEncoding('utf-8');
 
+var emitter = new EventEmitter();
+
+emitter.on("beforeCommand", function(instruction) {
+  console.log('You wrote: ' + instruction + ', trying to run command');
+});
+
+emitter.on("afterCommand", function() {
+  console.log("Finished command");
+});
+
+
+
+process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function () {
   // console.log('What do you want: ');
   var input = process.stdin.read();
   if (input !== null) {
     var instruction = input.toString().trim();
+    emitter.emit('beforeCommand', instruction);
 
     if (Object.keys(process).includes(instruction.slice(1, instruction.length))) {
-      console.log(process[instruction]);
+      console.log(process[instruction.slice(1, instruction.length)]);
     } else {
       console.log('Not process key');
       switch (instruction) {
 
         case '/getOSinfo':
           OSinfo.print();
+          console.log("\n");
           break;
 
         case '/exitMe':
@@ -25,5 +40,7 @@ process.stdin.on('readable', function () {
         default:
           process.stdout.write('Wrong instruction!\n');
       }
-    }}
+    }
+    emitter.emit("afterCommand");
+  }
 });
