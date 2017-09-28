db4d7d5acd7d10c4e75789d78e0623d41f446d66
Piotr Iwaniuk <piwaniuk@esri.pl>
2017-09-28 13:12:03 +0200

    printing function output fixed

--- 13.5-npm-init/index.js
+++ 13.5-npm-init/index.js
@@ -1,46 +1,50 @@
-var EventEmitter = require("events").EventEmitter;
+var EventEmitter = require('events').EventEmitter;
 var OSinfo = require('./modules/OSInfo');
-
 
 var emitter = new EventEmitter();
 
-emitter.on("beforeCommand", function(instruction) {
+emitter.on('beforeCommand', function (instruction) {
   console.log('You wrote: ' + instruction + ', trying to run command');
 });
 
-emitter.on("afterCommand", function() {
-  console.log("Finished command");
+emitter.on('afterCommand', function () {
+  console.log('Finished command');
 });
-
-
 
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function () {
-  // console.log('What do you want: ');
+  console.log('Write command with "/"');
   var input = process.stdin.read();
   if (input !== null) {
     var instruction = input.toString().trim();
     emitter.emit('beforeCommand', instruction);
 
-    if (Object.keys(process).includes(instruction.slice(1, instruction.length))) {
-      console.log(process[instruction.slice(1, instruction.length)]);
-    } else {
-      console.log('Not process key');
-      switch (instruction) {
+    instruction = instruction.slice(1, instruction.length);
 
+    if (instruction === 'exit') {
+      process.stdout.write('Quiting app!\n');
+      process.exit();
+    } else if (Object.keys(process).includes(instruction)) {
+      var result = process[instruction];
+      console.log(typeof(result));
+
+      if (typeof(result) == 'function') {
+        console.log(result = result());//JSON.stringify(result()));
+      } else {
+        console.log(process[instruction]);
+      }
+    } else {
+      console.log('Not a process key');
+      switch (instruction) {
         case '/getOSinfo':
           OSinfo.print();
-          console.log("\n");
+          console.log('\n');
           break;
-
-        case '/exitMe':
-          process.stdout.write('Quiting app!\n');
-          process.exit();
 
         default:
           process.stdout.write('Wrong instruction!\n');
       }
     }
-    emitter.emit("afterCommand");
+    emitter.emit('afterCommand');
   }
 });
