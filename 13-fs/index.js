c00c216746fbd22e8442b0775c8f2a97f795644f
piotr <pyhivo@gmail.com>
2017-10-02 08:46:28 +0200

    13.7 init

--- 13-fs/index.js
+++ 13-fs/index.js
@@ -1,12 +1,49 @@
 var fs = require('fs');
 var StatMode = require('stat-mode');
+var colors = require('colors');
 
-fs.stat('./img/cat.jpg', function(err, stats) {
-   var statMode = new StatMode(stats);
-   // console.log(stats);
-   console.log(statMode.toString());
-});
 
-fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
-   console.log(data);
-});
+const printImageProperties = () => {
+   fs.stat('./img/cat.jpg', function (err, stats) {
+      var statMode = new StatMode(stats);
+      console.log(stats);
+      console.log(statMode.toString());
+   });
+};
+
+const readWriteTextFile = () => {
+   fs.readFile('./tekst.txt', 'utf-8', function (err, data) {
+      console.log(data);
+   });
+
+   fs.writeFile('./tekst.txt', 'Tekst, który.txt', function (err) {
+      if (err) throw err;
+      console.log("Udało się!");
+   });
+};
+
+const appendTextToFile = () => {
+   fs.readFile('./tekst.txt', 'utf-8', function (err, data) {
+      console.log('Dane przed zapisaem');
+      console.log(data.blue);
+      // fs.writeFile('./tekst.txt', 'A tak wyglądają dane po zapisie', function (err) {
+      fs.appendFile('./tekst.txt', '\nA tak wyglądają dane po zapisie', function (err) {
+         if (err) throw err;
+         fs.readFile('./tekst.txt', 'utf-8', function (err, data) {
+            console.log("Dane po zapisie");
+            console.log(data.red);
+         });
+      });
+   });
+};
+
+
+
+const readDirectory = () => {
+   fs.readdir('./', function (err, files) {
+      console.log(files);
+      fs.writeFile('./directoryContent.txt', files.join("\n"));
+   });
+};
+
+readDirectory();
