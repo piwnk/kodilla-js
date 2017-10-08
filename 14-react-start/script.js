e82afc6bf88cab267e951186553ee3959ccd647e
piotr <pyhivo@gmail.com>
2017-10-08 16:24:01 +0200

    minor changes

--- 14-react-start/script.js
+++ 14-react-start/script.js
@@ -1,57 +1,40 @@
-const movies = [{
+const movies = [
+   {
       title: 'Harry Potter',
       desc: 'Film o czarodzieju',
       img: 'harry-potter.jpg'
-   },
-   {
+   }, {
       title: 'Król Lew',
       desc: 'Film o królu sawanny',
       img: 'lion-king.jpg'
-   },
-   {
+   }, {
       title: 'Podziemny krąg',
       desc: 'Film o niczym',
       img: 'fight-club.jpg'
    }
 ];
 
-
-movies.map(movie => {
+movies.map((movie) => {
    movie.id = movies.indexOf(movie) + 1;
 });
 
-
-const moviesElements = movies.map(movie => {
+const moviesElements = movies.map((movie) => {
    return React.createElement('li', {
-         key: movie.id
-      },
-      React.createElement('h2', {}, movie.title),
-      React.createElement('p', {}, movie.desc),
-      React.createElement('img', {
-         src: 'img/' + movie.img
-      })
-   );
+      key: movie.id
+   }, React.createElement('h2', {}, movie.title),
+   React.createElement('p', {}, movie.desc), React.createElement('img', {
+      src: 'img/' + movie.img
+   }));
 });
 
 // const element = React.createElement('div', {}, 'Hello world!');
 
-const element = React.createElement('div', {},
-   React.createElement('h1', {}, 'Lista filmów'),
-   // React.createElement('ul', {},
-   //    React.createElement('li', {},
-   //       React.createElement('h2', {}, 'Harry Potter'),
-   //       React.createElement('p', {}, 'Film o czarodzieju')
-   //    ),
-   //    React.createElement('li', {},
-   //       React.createElement('h2', {}, 'Król lew'),
-   //       React.createElement('p', {}, 'Film o królu sawanny')
-   //    )
-   // )
-   React.createElement('ul', {}, moviesElements)
-);
-
-
-
-
+const element = React.createElement('div', {}, React.createElement('h1', {}, 'Lista filmów'),
+// React.createElement('ul', {},    React.createElement('li', {},
+// React.createElement('h2', {}, 'Harry Potter'),       React.createElement('p',
+// {}, 'Film o czarodzieju')    ),    React.createElement('li', {},
+// React.createElement('h2', {}, 'Król lew'),       React.createElement('p', {},
+// 'Film o królu sawanny')    ) )
+React.createElement('ul', {}, moviesElements));
 
 ReactDOM.render(element, document.getElementById('app'));
