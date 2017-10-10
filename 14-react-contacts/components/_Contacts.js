1eeb62a0c0a631d27fa9442e8de5111ebe893ca0
Piotr Iwaniuk <piwaniuk@esri.pl>
2017-10-10 11:42:24 +0200

    working

---
+++ 14-react-contacts/components/_Contacts.js
@@ -0,0 +1,15 @@
+const Contacts = React.createClass({
+  propTypes: {
+    items: React.PropTypes.array.isRequired
+  },
+  render: function () {
+    const contacts = this.props.items.map(function (contact) {
+      return React.createElement(
+        Contact, {
+          item: contact,
+          key: contact.id
+        });
+    });
+    return React.createElement('ul', {className: 'contactsList'}, contacts);
+  }
+});
